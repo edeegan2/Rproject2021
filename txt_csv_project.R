b3afd3e66e01c104c5518c7c23ef1b51cb28c6df
jackboyle22 <67076115+jackboyle22@users.noreply.github.com>
2021-12-03 11:11:50 -0500

    Delete txt_csv_project.R

--- txt_csv_project.R
+++
@@ -1,9 +0,0 @@
-### TRY THIS CODE ###
-
-setwd("/Users/jackboyle/Documents/BioComputing/Rproject2021/countryY")
-FILE<-list.files( pattern = ".txt")
-for(i in 1:length(FILE)){
-  data.csv<-read.table(FILE[i], header=TRUE)
-  write.csv(data.csv, file=FILE[i], row.names=FALSE)
-}
-
